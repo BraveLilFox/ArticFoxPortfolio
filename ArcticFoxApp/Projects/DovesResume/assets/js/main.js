302a666e01401f3ca1230f714e36480e691d2092
BraveLilFox <157544440+BraveLilFox@users.noreply.github.com>
2024-04-30 17:28:35 -0400

    Update main.js

--- ArcticFoxApp/Projects/DovesResume/assets/js/main.js
+++ ArcticFoxApp/Projects/DovesResume/assets/js/main.js
@@ -4,4 +4,6 @@ const selectElement = selector => {
   if(element) return element;
   throw new Error(`Something went wrong, make sure that ${selector} exists or is typed correctly.`);
 };
+
+console.log(selectElement('.navbar'));
 // Nav Styles on scroll
