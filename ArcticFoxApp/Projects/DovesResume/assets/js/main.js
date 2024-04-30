8f713e085fafc3ef65811ed5aa2298c235e53d85
BraveLilFox <157544440+BraveLilFox@users.noreply.github.com>
2024-04-30 17:24:44 -0400

    Update main.js

--- ArcticFoxApp/Projects/DovesResume/assets/js/main.js
+++ ArcticFoxApp/Projects/DovesResume/assets/js/main.js
@@ -5,13 +5,3 @@ const selectElement = selector => {
   throw new Error(`Something went wrong, make sure that ${selector} exists or is typed correctly.`);
 };
 // Nav Styles on scroll
-const scrollHeader = () => {
-  const headerElement = selectElement('#header');
-  if(this.scrollY >= 15){
-    headerElement.classList.add('activated');
-  }else{
-    headerElement.classList.remove('activated');
-  }
-};
-
-window.addEventListener('scroll', scrollHeader);
