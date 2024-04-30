436df93675c269a5fd1a7d52ccf85adca705870d
BraveLilFox <157544440+BraveLilFox@users.noreply.github.com>
2024-04-30 17:04:52 -0400

    Update main.js

--- ArcticFoxApp/Projects/DovesResume/assets/js/main.js
+++ ArcticFoxApp/Projects/DovesResume/assets/js/main.js
@@ -2,7 +2,7 @@
 const selectElement = selector => {
   const element = document.querySelector(selector)
   if(element) return element;
-  throw new Error('Something went wrong, make sure that ${selector} exists or is typed correctly.');
+  throw new Error(`Something went wrong, make sure that ${selector} exists or is typed correctly.`);
 };
 // Nav Styles on scroll
 const scrollHeader = () => {
