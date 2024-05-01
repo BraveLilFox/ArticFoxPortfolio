7fdd44ed7ba689b383172388821a74af0805d74a
BraveLilFox <157544440+BraveLilFox@users.noreply.github.com>
2024-04-30 21:19:40 -0400

    Update main.js

--- ArcticFoxApp/Projects/DovesResume/assets/js/main.js
+++ ArcticFoxApp/Projects/DovesResume/assets/js/main.js
@@ -35,6 +35,11 @@ menuToggleIcon.addEventListener('click', toggleMenu);
 // Switch theme/add to local storage
 const bodyElement = document.body;
 const toggleThemeBtn = selectElement('#theme-toggle-btn');
+const currentTheme = localStorage.getItem('currentTheme');
+
+if(currentTheme){
+  bodyElement.classList.add('light-theme');
+}
 
 toggleThemeBtn.addEventListener('click', () =>{
   bodyElement.classList.toggle('light-theme');
