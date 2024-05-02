9a7c0c10b0a07cc042ed68e98e3a37f95a9e8bdf
Kaide Ryder <157544440+BraveLilFox@users.noreply.github.com>
2024-05-02 19:55:19 -0400

    Update main.js

--- ArcticFoxApp/assets/js/main.js
+++ ArcticFoxApp/assets/js/main.js
@@ -1,1 +1,6 @@
-javaScript
+// Grab Elements For JavaScript
+const selectElement = selector => {
+  const element = document.querySelector(selector);
+  if(element) return element;
+  throw new Error(`Somethings not right, make sure that ${selector} actually exists or that you spelled it correctly`)
+};
