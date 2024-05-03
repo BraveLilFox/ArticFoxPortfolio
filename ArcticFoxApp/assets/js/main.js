993a9ac4110e4bb87584a76e631cb5475d4368f4
Kaide Ryder <157544440+BraveLilFox@users.noreply.github.com>
2024-05-02 23:21:44 -0400

    Update main.js

--- ArcticFoxApp/assets/js/main.js
+++ ArcticFoxApp/assets/js/main.js
@@ -1,21 +1,15 @@
 
-// Grab Elements For JavaScript
-const selectElement = selector => {
-  const element = document.querySelector(selector);
-  if(element) return element;
-  throw new Error(`Somethings not right, make sure that ${selector} actually exists or that you spelled it correctly`);
-};
-
 // Defining Elements
 
 const sections = document.querySelectorAll('.section');
-const sectionBtns = document.querySelectorAll('.control');
 const btnSection = document.querySelectorAll('.controls');
+const sectionBtns = document.querySelectorAll('.control');
 const allSections = document.querySelector.body;
 
 function PageTransitions(){
+  //Button click active class
   for(let i = 0; i < sectionBtns.length; i++){
-    sectionBtns[i].addListener('click', function(){
+    sectionBtns[i].addEventListener('click', function(){
       let currentBtn = document.querySelectorAll('.active-btn');
       currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
       this.className + 'active-btn';
