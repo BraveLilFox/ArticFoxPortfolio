afd96970ff7ed4aecdd972545b5c69067e3021b2
Kaide Ryder <157544440+BraveLilFox@users.noreply.github.com>
2024-05-03 15:31:30 -0400

    Update main.js

--- ArcticFoxApp/assets/js/main.js
+++ ArcticFoxApp/assets/js/main.js
@@ -22,8 +22,17 @@ function PageTransitions(){
     if(id){
       //remove selected from the other btns
       sectionBtns.forEach((btn) => {
-        
+        btn.classList.remove('active')
       })
+      e.target.classList.add('active')
+
+      // Hide other sections
+      sections.forEach((section)=>{
+        sections.classList.remove('active')
+      })
+
+      const element = document.getElementById(id);
+      element.classList.add('active');
     }
   })
 }
