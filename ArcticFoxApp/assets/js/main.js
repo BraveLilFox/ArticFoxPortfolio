7f8190b5427a2f53626a0d277d59986f5cc1cff3
Kaide Ryder <157544440+BraveLilFox@users.noreply.github.com>
2024-05-02 22:59:21 -0400

    Update main.js

--- ArcticFoxApp/assets/js/main.js
+++ ArcticFoxApp/assets/js/main.js
@@ -3,7 +3,7 @@
 const selectElement = selector => {
   const element = document.querySelector(selector);
   if(element) return element;
-  throw new Error(`Somethings not right, make sure that ${selector} actually exists or that you spelled it correctly`)
+  throw new Error(`Somethings not right, make sure that ${selector} actually exists or that you spelled it correctly`);
 };
 
 // Defining Elements
