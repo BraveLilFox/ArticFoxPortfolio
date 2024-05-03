257031f2725172f720bd6f28f38ff0140ff6dd35
Kaide Ryder <157544440+BraveLilFox@users.noreply.github.com>
2024-05-02 20:11:26 -0400

    Update main.js

--- ArcticFoxApp/assets/js/main.js
+++ ArcticFoxApp/assets/js/main.js
@@ -4,3 +4,8 @@ const selectElement = selector => {
   if(element) return element;
   throw new Error(`Somethings not right, make sure that ${selector} actually exists or that you spelled it correctly`)
 };
+
+// Defining Elements
+
+const sections = selectElement{'.sections'};
+const controlBtns = selectElement{'.control'};
