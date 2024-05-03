fe091be3302fd6d7e9087cbd113771a6cbd11a80
Kaide Ryder <157544440+BraveLilFox@users.noreply.github.com>
2024-05-02 23:40:32 -0400

    Update main.js

--- ArcticFoxApp/assets/js/main.js
+++ ArcticFoxApp/assets/js/main.js
@@ -13,9 +13,9 @@ function PageTransitions(){
       let currentBtn = document.querySelectorAll('.active-btn');
       currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
       this.className += ' active-btn';
-    });
-  };
-};
+    })
+  }
+}
 
 PageTransitions();
 
