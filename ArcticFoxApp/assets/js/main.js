7b79686d5eca8278b2cf5dfd13c7f2ecb71dec7f
Kaide Ryder <157544440+BraveLilFox@users.noreply.github.com>
2024-05-02 21:46:45 -0400

    Update main.js

--- ArcticFoxApp/assets/js/main.js
+++ ArcticFoxApp/assets/js/main.js
@@ -5,22 +5,28 @@ const selectElement = selector => {
   throw new Error(`Somethings not right, make sure that ${selector} actually exists or that you spelled it correctly`)
 };
 
+const selectMultiple = selectorMult => {
+  const elements = document.querySelectAll{'selectorMult'};
+  if (elements) return elements;
+  throw new Error(`Somethings not right, make sure that ${selectorMult} actually exists or that you spelled it correctly`)
+};
+
 // Defining Elements
 
-const sections = selectElement{'.section'};
-const sectionBtns = selectElement{'.control'};
-const btnSection = selectElement{'.controls'};
+const sections = selectMultiple{'.section'};
+const sectionBtns = selectMultiple{'.control'};
+const btnSection = selectMultiple{'.controls'};
 const allSections = document.querySelector.body;
 
 function PageTransitions(){
   for(let i =0; i < sectionBtns.length; i++){
     sectionBtns[i].addListener('click', function(){
-      let currentBtn = selectElement('.active-btn');
+      let currentBtn = selectMultiple('.active-btn');
       currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
       this.className + 'active-btn';
     });
-  }
-}
+  };
+};
 
 PageTransitions();
 
